37280a3b28a58dcc10b58fd7c8e7a96a1e5644d7
Conner Nilsen <connernilsen@Conners-MBP.mineraltree.com>
2020-01-13 16:39:21 -0500

    Adjust starting script

--- start.py
+++ start.py
@@ -6,11 +6,10 @@ sections = ["accounts", "inventory", "products", "reviews"]#, "server"]
 for sect in range(len(sections)):
     name = sections[sect]
     if (len(sys.argv) == 1 or len(sys.argv) > 1 and sys.argv[1] == "-r"):
-        if (len(sys.argv) == 1):
-            os.system(f'docker image build -t {name}-test:1.0 {name}/')
+        os.system(f'docker image build -t {name}-test:1.0 {name}/')
 
         #if (name != "server"):
-            os.system(f'docker container run --publish 400{sect + 1}:400{sect + 1} --detach --name {name}-test {name}-test:1.0')
+        os.system(f'docker container run --publish 400{sect + 1}:400{sect + 1} --detach --name {name}-test {name}-test:1.0')
         #else:
         #    os.system('docker container run --publish 4000:4000 --detach --name server-test server-test:1.0')
 
